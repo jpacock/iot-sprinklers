8a7a953e66c9256617d58ff778e7cfefe8a4cb5d
jpacock <jpacock@us.ibm.com>
2020-03-04 15:30:01 -0600

    change gpio pin

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var rpio = require('rpio');
 
+rpio.open(8, rpio.OUTPUT, rpio.LOW); // GPIO 14 
+
 for (var i = 0; i < 5; i++) {
-    rpio.write(14, rpio.HIGH);
+    rpio.write(8, rpio.HIGH);
     rpio.sleep(1);
     
-    rpio.write(14, rpio.LOW);
+    rpio.write(8, rpio.LOW);
     rpio.msleep(500);
 }
