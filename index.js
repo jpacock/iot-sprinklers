38eeafb8f458c9376581172c092b2921c4045935
Jordan Acock <Jordan.Acock@gmail.com>
2020-03-04 23:13:51 +0000

    add webpack dependencies

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ var rpio = require('rpio');
 
 rpio.open(8, rpio.OUTPUT, rpio.LOW); // GPIO 14 
 
-for (var i = 0; i < 5; i++) {
-    rpio.write(8, rpio.HIGH);
-    rpio.sleep(1);
-    
-    rpio.write(8, rpio.LOW);
-    rpio.msleep(500);
-}
+rpio.write(8, rpio.LOW);
+rpio.sleep(20);
+
+rpio.write(8, rpio.HIGH);
