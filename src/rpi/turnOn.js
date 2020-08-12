87a9da4f0d600d435bfae3d7d0ee2c239f1b7b2d
jpacock <jpacock@us.ibm.com>
2020-08-12 09:04:14 -0500

    Move rpi calls to isolated function

--- src/rpi/turnOn.js
+++ src/rpi/turnOn.js
@@ -1,3 +1,11 @@
-module.exports.turnOn = () => {
+const turnOn = (duration) => {
+  const rpio = require('rpio');
+  rpio.open(8, rpio.OUTPUT, rpio.HIGH);
+  rpio.write(8, rpio.LOW);
+  rpio.sleep(duration);
+  rpio.write(8, rpio.HIGH);
+};
 
+module.exports = {
+  turnOn
 };
--- src/services/runZone.js
+++ src/services/runZone.js
@@ -1,9 +1,7 @@
+const { turnOn } = require('../rpi/turnOn');
+
 const runZone = (zoneId, duration) => {
-  const rpio = require('rpio');
-  rpio.open(8, rpio.OUTPUT, rpio.HIGH);
-  rpio.write(8, rpio.LOW);
-  rpio.sleep(duration);
-  rpio.write(8, rpio.HIGH);
+  turnOn(duration);
 };
 
 module.exports = {
