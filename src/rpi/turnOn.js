c73a128dd104f0ec511fa352d109cdcf2b96a703
jpacock <jpacock@us.ibm.com>
2020-11-30 10:08:27 -0600

    change rpio sleep to setTimeout() to avoid block

--- src/rpi/turnOn.js
+++ src/rpi/turnOn.js
@@ -2,8 +2,9 @@ const turnOn = (duration) => {
   const rpio = require('rpio');
   rpio.open(8, rpio.OUTPUT, rpio.HIGH);
   rpio.write(8, rpio.LOW);
-  rpio.sleep(duration);
-  rpio.write(8, rpio.HIGH);
+  // rpio.sleep(duration);
+  setTimeout(() => { rpio.write(8, rpio.HIGH) }, duration)
+  // rpio.write(8, rpio.HIGH);
 };
 
 module.exports = {
