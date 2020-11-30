5588261dfa06134f5d5f13911b726d301ee3bd20
jpacock <jpacock@us.ibm.com>
2020-11-30 10:14:11 -0600

    update

--- src/rpi/turnOn.js
+++ src/rpi/turnOn.js
@@ -3,8 +3,9 @@ const turnOn = (duration) => {
   const durationInMilliseconds = duration * 1000;
   rpio.open(8, rpio.OUTPUT, rpio.HIGH);
   rpio.write(8, rpio.LOW);
+  console.log('turned on');
   // rpio.sleep(duration);
-  setTimeout(() => { rpio.write(8, rpio.HIGH) }, durationInMilliseconds)
+  setTimeout(() => { rpio.write(8, rpio.HIGH); console.log('turned off') }, durationInMilliseconds)
   // rpio.write(8, rpio.HIGH);
 };
 
