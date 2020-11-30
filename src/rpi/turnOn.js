2a3d07c3ea65ffbcfede8c845f533b8e1705c0b8
jpacock <jpacock@us.ibm.com>
2020-11-30 10:10:51 -0600

    change setTimeout to account for ms

--- src/rpi/turnOn.js
+++ src/rpi/turnOn.js
@@ -1,9 +1,10 @@
 const turnOn = (duration) => {
   const rpio = require('rpio');
+  const durationInMilliseconds = duration * 1000;
   rpio.open(8, rpio.OUTPUT, rpio.HIGH);
   rpio.write(8, rpio.LOW);
   // rpio.sleep(duration);
-  setTimeout(() => { rpio.write(8, rpio.HIGH) }, duration)
+  setTimeout(() => { rpio.write(8, rpio.HIGH) }, durationInMilliseconds)
   // rpio.write(8, rpio.HIGH);
 };
 
