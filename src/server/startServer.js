63d128374b92c99b1cbad2906ee7b80504ba3031
jpacock <jpacock@us.ibm.com>
2021-09-19 22:30:45 -0500

    create schedule

--- src/server/startServer.js
+++ src/server/startServer.js
@@ -17,10 +17,25 @@ const startServer = () => {
   server.post('/zones/:zoneId/on', (req, res) => handleTurnZoneOn(req, res));
 
   const CronJob = require('cron').CronJob;
-  const job = new CronJob('0 0 5 * * 2,6', () => {
-    runZone(1, 2400);
+  const job1 = new CronJob('0 0 5 * * 1,5', () => {
+    runZone(1, 2399);
   }, null, true, 'America/Chicago');
-  job.start();
+  job1.start();
+
+  const job2 = new CronJob('0 40 5 * * 1,5', () => {
+    runZone(2, 1799);
+  }, null, true, 'America/Chicago');
+  job2.start();
+
+  const job3 = new CronJob('0 10 6 * * 1,5', () => {
+    runZone(3, 1799);
+  }, null, true, 'America/Chicago');
+  job3.start();
+
+  const job4 = new CronJob('0 40 6 * * 1,5', () => {
+    runZone(4, 1799);
+  }, null, true, 'America/Chicago');
+  job4.start();
 
   server.listen(3000, () =>
     console.log('Server listening on port 3000!'),
