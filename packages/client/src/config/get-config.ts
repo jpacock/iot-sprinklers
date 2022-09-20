a6b62593f19371e98fe06a2b11f28157b8488742
Jordan Acock <jordan@jordans-mbp.lan>
2022-09-20 10:38:16 -0500

    Update git

--- packages/client/src/config/get-config.ts
+++ packages/client/src/config/get-config.ts
@@ -1,6 +1,6 @@
 export function getConfig() {
   return {
-    // sprinklerServiceUrl: 'http://136.37.159.219:3000',
-    sprinklerServiceUrl: 'http://127.0.0.1:3000',
+    sprinklerServiceUrl: 'http://136.37.159.219:3000',
+    // sprinklerServiceUrl: 'http://127.0.0.1:3000',
   }
 }
