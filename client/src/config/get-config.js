63e86ef702efea21a99bfbd2eeaec480368cf992
Jordan Acock <jpacock@us.ibm.com>
2023-04-01 13:56:06 -0500

    Update client

--- client/src/config/get-config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.getConfig = void 0;
-function getConfig() {
-    return {
-        sprinklerServiceUrl: 'http://136.37.159.219:3000'
-    };
-}
-exports.getConfig = getConfig;
